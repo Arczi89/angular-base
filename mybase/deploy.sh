456a797f286e6419f6619c848d021a76dd31131a
artur.szwagrzak <artur@szwagrzak.pl>
2025-08-01 12:26:54 +0200

    feat: add deploy configuration

---
+++ mybase/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USER='arturszwagrzak'
+HOST='arturszwagrzak.atthost24.pl'
+REMOTE_DIR='/home/arturszwagrzak/websites/mybase/'
+PORT=6022
+
+echo "Uploading files to $USER@$HOST:$REMOTE_DIR"
+whoami
+rsync -az -e "ssh -p $PORT" --delete dist/mybase/browser/ $USER@$HOST:$REMOTE_DIR
+ssh -p $PORT $USER@$HOST "cp $REMOTE_DIR/../.env $REMOTE_DIR"
