7376e88ba00308e77fb66dacaa1d9565e7fa49ea
artur.szwagrzak <artur@szwagrzak.pl>
2025-08-01 14:57:02 +0200

    fix deploy problems

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ PORT=6022
 
 echo "Uploading files to $USER@$HOST:$REMOTE_DIR"
 whoami
-rsync -az -e "ssh -p $PORT" --delete dist/mybase/ $USER@$HOST:$REMOTE_DIRclkea
+rsync -az -e "ssh -p $PORT" --exclude='.protected' --exclude='.bash_history' --exclude='logs' --exclude='sockets' --exclude='websites' dist/mybase/ $USER@$HOST:$REMOTE_DIR
 ssh -p $PORT $USER@$HOST "cp $REMOTE_DIR/../.env $REMOTE_DIR"
