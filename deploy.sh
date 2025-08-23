bcfc1666b57705c9b41914bc814d75cf5e52d1d5
artur.szwagrzak <artur@szwagrzak.pl>
2025-08-24 00:23:46 +0200

    update deploy.sh to new hosting, fix gitignore file

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,9 @@ PORT=6022
 
 echo "Uploading files to $USER@$HOST:$REMOTE_DIR"
 whoami
-rsync -az -e "ssh -p $PORT" --exclude='.protected' --exclude='.bash_history' --exclude='logs' --exclude='sockets' --exclude='websites' dist/mybase/ $USER@$HOST:$REMOTE_DIR
-ssh -p $PORT $USER@$HOST "cp $REMOTE_DIR/../.env $REMOTE_DIR"
+
+# Bezpieczne użycie --delete w dedykowanym folderze mybase
+rsync -az -e "ssh -p $PORT" --delete dist/mybase/ $USER@$HOST:$REMOTE_DIR
+
+# Kopiuj .env-angular-base do .env w folderze aplikacji
+ssh -p $PORT $USER@$HOST "cp $REMOTE_DIR/../.env-angular-base $REMOTE_DIR/.env"
