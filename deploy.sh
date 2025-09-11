ceb02f2083826bfb7423f961c19321007b539ccd
artur.szwagrzak <artur@szwagrzak.pl>
2025-09-11 15:03:21 +0200

    remove mybase folder

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,12 @@
 
 USER='dm77338'
 HOST='dm77338.domenomania.eu'
-REMOTE_DIR='/home/dm77338/demo.szwagrzak.pl/mybase/'
+REMOTE_DIR='/home/dm77338/demo.szwagrzak.pl/'
 PORT=22
 
 echo "Uploading files to $USER@$HOST:$REMOTE_DIR"
 whoami
 
-# Bezpieczne użycie --delete w dedykowanym folderze mybase
-rsync -az -e "ssh -p $PORT" --delete dist/mybase/ $USER@$HOST:$REMOTE_DIR
+rsync -az -e "ssh -p $PORT" --delete dist/ $USER@$HOST:$REMOTE_DIR
 
-# Kopiuj .env-angular-base do .env w folderze aplikacji
 ssh -p $PORT $USER@$HOST "cp $REMOTE_DIR/../.env-angular-base $REMOTE_DIR/.env"
